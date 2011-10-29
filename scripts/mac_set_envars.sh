203f474e33b1aded79595f31d9b4e0761d9f6e58
Ernad Husremovic <hernad@bring.out.ba>
2011-10-29 14:20:37 +0200

    source mac envars

---
+++ scripts/mac_set_envars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH=$PATH:/opt/harbour/bin
+
+export HB_WITH_SQLITE3=external
+#export HB_WITH_QT=/usr/local/Trolltech/Qt-4.7.4/include
+export HB_COMPILER=gcc
+
+#HB_USER_CFLAGS="-arch x86_64"
+#HB_USER_LDFLAGS="-arch x86_64"
+
+
+export HB_INC_INSTALL=/opt/harbour/include/harbour
+export HB_LIB_INSTALL=/opt/harbour/lib/harbour

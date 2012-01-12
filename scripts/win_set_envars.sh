b782a81db94cbabc9705e2612c3cb920469bdc8b
Ernad Husremovic <hernad@bring.out.ba>
2012-01-12 19:44:24 +0100

    ovaj win_set_envars.sh uspjesno builda harbour 3.0.0 - hbqt i hbpgsq

--- scripts/win_set_envars.sh
+++ scripts/win_set_envars.sh
@@ -5,13 +5,37 @@
 export TEMP=/c/tmp
 export TMP=/c/tmp
 
-HB_ROOT=/c/harbour/b1
-export PATH=$HB_ROOT/bin:/c/PostgreSQL/9.1/include:/c/PostgreSQL/9.1/lib:$PATH
+export HB_ARCHITECTURE=win
+export HB_COMPILER=mingw
+
+
+HB_ROOT=/c/hbout
+
+export PATH=$HB_ROOT/bin:/c/MinGW/bin:/c/PostgreSQL/9.1/bin:$PATH
 export HB_INC_INSTALL=$HB_ROOT/include
 export HB_LIB_INSTALL=$HB_ROOT/lib/win/mingw
 
-HB_DBG=`pwd`
+export HB_INSTALL_PREFIX=$HB_ROOT
+
+export HB_WITH_QT=c:\\Qt\\4.7.4\\include
+export HB_WITH_PGSQL=c:\\PostgreSQL\\9.1\\include
+
+
+#export QT_DIR=/c/Qt/4.7.4
+#export QT_DIR=c:\\Qt\\4.7.4
+
+
+#export PATH=/c/Qt/4.7.4/bin:$PATH
+
+#export QT_INC_DIR=$QT_DIR\\include
+
+#export HB_WITH_QT=$QT_DIR\\bin
+#export HB_INC_QT=$QT_INC_DIR
+#export HB_LIB_QT=$QT_DIR\\lib
+
+HB_DBG=/c/github/F18_knowhow
+
 export HB_DBG_PATH=$HB_DBG/common:$HB_DBG/pos:$HB_DBG/kalk:$HB_DBG/fin:$HB_DBG/fakt:$HB_DBG/os:$HB_DBG/ld:/$HB_DBG/epdf:$HB_DBG/rnal
 
-echo "HB_DBG_PATH="  $HB_DBG_PATH
+echo HB_DBG_PATH=$HB_DBG_PATH
 

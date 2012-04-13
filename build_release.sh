6b6e7bdd3c8bfaab201fd54e50bc43f602b70fb7
Ernad Husremovic <hernad@bring.out.ba>
2012-04-13 14:50:40 +0200

    build_release.sh comment mac only

--- build_release.sh
+++ build_release.sh
@@ -31,27 +31,27 @@ cp hb_release.hbm hbmk.hbm
 
 hbmk2 F18.hbp -rebuildall
 
-mkdir -p out/bin
+#mkdir -p out/bin
 
 #rm out/bin/*
 #rm out/*
 
-cp -av F18 out/bin
-cp -av scripts/install_f18.sh out/
+#cp -av F18 out/bin
+#cp -av scripts/install_f18.sh out/
 
-cd out
+#cd out
 
-F18_REL=`grep 'F18_VER\ \\+\\"\\(.*\\)\\"' -o ../common/f18_ver.ch | grep -o "[0-9]\\+.[0-9]\\+.[0-9]\\+"`
+#F18_REL=`grep 'F18_VER\ \\+\\"\\(.*\\)\\"' -o ../common/f18_ver.ch | grep -o "[0-9]\\+.[0-9]\\+.[0-9]\\+"`
 
-echo $F18_REL
+#echo $F18_REL
 
-INSTALL_F_NAME=F18_${OS}_${F18_REL}_install.tar.bz2 
+#INSTALL_F_NAME=F18_${OS}_${F18_REL}_install.tar.bz2 
 
-echo kreiram $INSTALL_F_NAME
+#echo kreiram $INSTALL_F_NAME
 
-gnutar cvfj ../$INSTALL_F_NAME .
+#gnutar cvfj ../$INSTALL_F_NAME .
 
 echo "------- rezultat:  $INSTALL_F_NAME -------------"
 
-cd ..
-gnutar tvfj $INSTALL_F_NAME
+#cd ..
+#gnutar tvfj $INSTALL_F_NAME

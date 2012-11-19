f462a1331968fe07aababd6d629e6c0cf21e0588
Ernad Husremovic <hernad@bring.out.ba>
2012-11-19 16:21:31 +0100

    build_travis - F18 build

--- build_travis.sh
+++ build_travis.sh
@@ -35,3 +35,24 @@ export PATH=$PATH:$HB_INSTALL_PREFIX/bin
 cd harbour/harbour
 make
 make install
+
+cd ../../
+
+export KNOWHOW_ERP_ROOT=$CUR_DIR
+export HARBOUR_ROOT=$KNOWHOW_ERP_ROOT/hbout
+
+let KH_PATH=`echo $PATH | grep -c $KNOWHOW_ERP_ROOT/bin`
+
+if [[ $KH_PATH -eq 0 ]]; then
+     # echo "knowhowERP not in path ($KH_PATH) dodajem"
+     export PATH=$KNOWHOW_ERP_ROOT/bin:$KNOWHOW_ERP_ROOT/util:$HARBOUR_ROOT/bin:$PATH
+else
+     echo "knowhowERP already in path ($KH_PATH): $PATH"
+fi
+
+export HB_COMPILER=gcc
+
+export HB_INC_INSTALL=$HARBOUR_ROOT/include
+export HB_LIB_INSTALL=$HARBOUR_ROOT/lib
+
+. ./build_test.sh

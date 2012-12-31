535e215bae075e110033a9fe30bae2bc8c8eecfe
bringout <bringout@builder.(none)>
2012-12-31 12:57:27 +0100

    scripts/install_odt.sh

--- build_travis.sh
+++ build_travis.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
+source scripts/common.sh
 
-GCODE_URL_ROOT_F18=http://knowhow-erp-f18.googlecode.com/files
-GCODE_URL_ROOT=http://knowhow-erp.googlecode.com/files
-F18_INSTALL_ROOT=/opt/knowhowERP
-
-# export QT_DIR=c:/knowhowERP/Qt
-
-# export QT_DIR=c:\\Qt\\4.7.4
-# export QT_DIR_CYGWIN=/c/Qt/4.7.4
-
-# export HB_WITH_GTALLEGRO=no
-# export HB_WITH_ALLEGRO=no
-# export HB_WITH_GTWVG=yes
-
-
-
-
-CUR_USER=`whoami`
-CUR_DIR=`pwd`
-export HB_INSTALL_PREFIX=$CUR_DIR/hbout
-
-export HB_WITH_SQLITE3=yes
-
-
-export PATH=$PATH:$HB_INSTALL_PREFIX/bin
 
 function build_harbour {
 git submodule init
@@ -88,41 +65,6 @@ echo "CREATE database f18_test" | psql -U postgres
 psql -U postgres f18_test < test/data/f18_test.sql > create_databases.log
 }
 
-function install_jod_reports {
-
-D_FILE=jodreports-cli.jar
-wget -q -nc $GCODE_URL_ROOT/$D_FILE
-
-DEST="/opt/knowhowERP/util/"
-sudo mkdir -p $DEST
-sudo chown $CUR_USER $DEST
-sudo cp $D_FILE  $DEST
-
-DEST_TPL="/opt/knowhowERP/template/"
-sudo mkdir -p $DEST_TPL
-sudo chown $CUR_USER $DEST_TPL
-
-}
-
-function install_template {
-
-TPL_FILE=F18_template_${TPL_VER}
-
-rm ${TPL_FILE}.tar.bz2
-wget $GCODE_URL_ROOT_F18/${TPL_FILE}.tar.bz2
-
-tar -C $F18_INSTALL_ROOT -jxf ${TPL_FILE}.tar.bz2
-}
-
-function install_harbour {
-
-HRB_FILE=harbour_travis.tar.bz2
-
-rm ${HRB_FILE}
-wget $GCODE_URL_ROOT/${HRB_FILE}
-
-tar -C . -jxf $HRB_FILE
-}
 
 function run_tests {
 #./F18_test > F18.test.log
---
+++ scripts/install_odt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source scripts/common.sh
+
+
+install_jod_reports
+
+TPL_VER="1.2.6"
+install_template

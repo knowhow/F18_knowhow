06fa57d036a5eb809c76747cd059007d060a386e
unknown <bringout@bout-c413d23ab7.(none)>
2013-11-22 19:17:32 +0100

    f18_upd.bat vratio funkcionalnost

--- scripts/f18_upd.bat
+++ scripts/f18_upd.bat
@@ -7,20 +7,16 @@ set DEST=C:\knowhowERP\bin
 
 :SERVICE
 echo  "loop sa malom pauzom dok  se F18 ne zatvori"
-PING -n 3 www.google.com  >NUL
+PING -n 1 www.google.com  >NUL
 REM # provjeri dali se F18 vrti
 tasklist /FI "IMAGENAME eq F18.exe" 2>NUL | find /I /N "F18.exe">NUL
-if "%ERRORLEVEL%"=="0"   
-  goto :SERVICE   
-else 
-  GOTO :UPDATE
+if "%ERRORLEVEL%"=="0" goto :SERVICE  else GOTO :UPDATE
 
 :UPDATE
 
-if not exist %1 echo "F18 update fajl ne postoji"  
-  goto  :ERR 
+if not exist %1 echo "F18 update fajl ne postoji"  goto  :ERR 
 
-rem if %1 = 0 echo "F18 je 0"   goto :ERR
+if %1 == "0" echo "F18 je 0"   goto :ERR
 
 gzip -dNfc  < %1 > %DEST%\F18.exe 
 del /Q  %1 

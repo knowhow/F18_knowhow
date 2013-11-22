e96f4fa496e48b923e016e32844eeb497d5254ca
Sasa Vranic <vsasa@bring.out.ba>
2013-11-22 17:20:20 +0100

    korekcija f18_upd.bat skripte

--- scripts/f18_upd.bat
+++ scripts/f18_upd.bat
@@ -10,23 +10,24 @@ echo  "loop sa malom pauzom dok  se F18 ne zatvori"
 PING -n 3 www.google.com  >NUL
 REM # provjeri dali se F18 vrti
 tasklist /FI "IMAGENAME eq F18.exe" 2>NUL | find /I /N "F18.exe">NUL
-if "%ERRORLEVEL%"=="0"   goto :SERVICE   else GOTO :UPDATE
-
+if "%ERRORLEVEL%"=="0"   
+  goto :SERVICE   
+else 
+  GOTO :UPDATE
 
 :UPDATE
 
-if not exist %1 echo "F18 update fajl ne postoji"  goto  :ERR 
+if not exist %1 echo "F18 update fajl ne postoji"  
+  goto  :ERR 
+
 rem if %1 = 0 echo "F18 je 0"   goto :ERR
 
 gzip -dNfc  < %1 > %DEST%\F18.exe 
 del /Q  %1 
 goto :END
 
-
-
 :ERR
 echo "Prekidam operaciju UPDATE-a"
 
 :END
-
 echo "Update OK"

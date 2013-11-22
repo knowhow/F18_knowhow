b271e197cff2ad2f8f3960298a1abd26f81a27bd
Jasmin Beganovic <bjasko@bring.out.ba>
2013-11-22 20:02:05 +0100

    f18_upd.bat vratio provjera download fajla

--- scripts/f18_upd.bat
+++ scripts/f18_upd.bat
@@ -6,7 +6,7 @@ set PATH=%PATH%;C:\knowhowERP\bin;C:\knowhowERP\lib;C:\knowhowERP\util
 set DEST=C:\knowhowERP\bin
 
 :SERVICE
-echo  "loop sa malom pauzom dok  se F18 ne zatvori"
+echo  "Provjeravam dali je F18 zatvoren"
 PING -n 1 www.google.com  >NUL
 REM # provjeri dali se F18 vrti
 tasklist /FI "IMAGENAME eq F18.exe" 2>NUL | find /I /N "F18.exe">NUL
@@ -14,9 +14,11 @@ if "%ERRORLEVEL%"=="0" goto :SERVICE  else GOTO :UPDATE
 
 :UPDATE
 
-if not exist %1 echo "F18 update fajl ne postoji"  goto  :ERR 
-
-if %1 == "0" echo "F18 je 0"   goto :ERR
+if not exist %1 echo "F18 update fajl ne postoji"  
+goto :ERR 
+if %z1 eq 0 
+del %1 
+goto :ERR
 
 gzip -dNfc  < %1 > %DEST%\F18.exe 
 del /Q  %1 
@@ -24,6 +26,13 @@ goto :END
 
 :ERR
 echo "Prekidam operaciju UPDATE-a"
+exit /b
 
 :END
-echo "Update OK"
+echo ""
+echo ""
+echo "UPDATE OK"
+echo ""
+echo ""
+PING -n 2 www.google.com  >NUL
+exit /b

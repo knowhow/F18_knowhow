ef9a536b8348a152cbcb6e09087c01a600c31a0e
Jasmin Beganović <bjasko@bring.out.ba>
2013-11-22 10:11:58 +0100

    init update ubuntu

--- scripts/f18_upd.sh
+++ scripts/f18_upd.sh
@@ -1,1 +1,24 @@
-skripta za unix update
+#!/bin/bash
+
+PATH=/bin:/usr/bin:/usr/local/bin:/opt/knowhowERP/bin:/opt/knowhowERP/util
+VER=1.0.0
+DAT=22.11.2013
+SERVICE=$(ps ax | grep -v grep | grep -c F18) 
+DEST=/opt/knowhowERP/bin
+
+sleep 3
+
+while  [ "$SERVICE" -gt 0 ]
+	do	
+    	echo "$SERVICE je pokrenut cekam da se zatvori"
+        sleep 5  
+    done
+    	gzip -dNf $1
+    	mv F18 $DEST
+    	chmod +x $FILE
+    	echo "update je zavrsen"
+fi
+
+
+exit 0
+

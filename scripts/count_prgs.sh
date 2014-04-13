bec7ed84a1fad60e0f34f8cbdd11f9500c12f396
Ernad Husremovic <hernad@bring.out.ba>
2014-04-13 09:00:51 +0200

    broj prg-ova unutar projekata

---
+++ scripts/count_prgs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function count_prgs() {
+
+   NUM=`find $1 -name "*.prg" | grep -c ^$1`
+   echo "$1: $NUM"
+}
+
+count_prgs common
+count_prgs fin
+count_prgs kalk
+count_prgs fakt
+count_prgs pos
+count_prgs os
+count_prgs ld
+count_prgs virm
+count_prgs mat
+count_prgs kadev
+
+

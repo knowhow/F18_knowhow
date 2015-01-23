450583a9951e531a578b1ab09b3dc542bfe37a1e
Ernad Husremovic <hernad@bring.out.ba>
2015-01-23 16:52:22 +0100

    scripts iz master

--- scripts/ubuntu_set_envars.sh
+++ scripts/ubuntu_set_envars.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-export KNOWHOW_ERP_ROOT=/opt/knowhowERP
-export HARBOUR_ROOT=$KNOWHOW_ERP_ROOT/hbout
-export HB_ROOT=$KNOWHOW_ERP_ROOT/hbout
-export PATH=$HB_ROOT/bin:$PATH
+export LX_UBUNTU=1
 
-let KH_PATH=`echo $PATH | grep -c $KNOWHOW_ERP_ROOT/bin`
+. scripts/linux.sh
 
-if [[ $KH_PATH -eq 0 ]]; then
-   # echo "knowhowERP not in path ($KH_PATH) dodajem"
-   export PATH=$KNOWHOW_ERP_ROOT/bin:$KNOWHOW_ERP_ROOT/util:$HARBOUR_ROOT/bin:$PATH
-else
-   echo "knowhowERP already in path ($KH_PATH): $PATH"
-fi
+. scripts/set_envars.sh
 
-export HB_COMPILER=gcc
+for m in $MODULES
+do
+    HB_DBG_PATH="$HB_DBG_PATH:$HB_DBG/$m"
+done
 
-export HB_INC_INSTALL=$HARBOUR_ROOT/include/harbour
-export HB_LIB_INSTALL=$HARBOUR_ROOT/lib/harbour
-
-
-HB_DBG=`pwd`
-export HB_DBG_PATH=$HB_DBG/test:$HB_DBG/common:$HB_DBG/pos:$HB_DBG/kalk:$HB_DBG/fin:$HB_DBG/fakt:$HB_DBG/os:$HB_DBG/ld:/$HB_DBG/epdv:$HB_DBG/rnal:$HB_DBG/mat:$HB_DBG/virm:$HB_DBG/reports:$HB_DBG/kadev
-
-
+export HB_DBG_PATH
 echo "HB_DBG_PATH="  $HB_DBG_PATH
+
+

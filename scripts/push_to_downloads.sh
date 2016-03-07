42a00cfb3fef2bb12d50c483df34d5df8e11d73d
Ernad Husremovic <hernad@bring.out.ba>
2016-03-07 16:00:51 +0100

    green-1

--- scripts/push_to_downloads.sh
+++ scripts/push_to_downloads.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SSH_OPTS="-i $HOME/hernad_ssh.key -o StrictHostKeyChecking=no"
-WWW_PATH=/var/www/files
+#SSH_OPTS="-i $HOME/hernad_ssh.key -o StrictHostKeyChecking=no"
+#WWW_PATH=/var/www/files
 
-scp $SSH_OPTS $1 root@download.bring.out.ba:$WWW_PATH/
-ssh $SSH_OPTS root@download.bring.out.ba chmod +r $WWW_PATH/*
+#scp $SSH_OPTS $1 root@download.bring.out.ba:$WWW_PATH/
+#ssh $SSH_OPTS root@download.bring.out.ba chmod +r $WWW_PATH/*
+
+scp $1 green-1:/data/f18-downloads/downloads.bring.out.ba/www/files/
+

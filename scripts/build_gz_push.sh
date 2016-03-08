04a637b1456ad55fb404cbb22673296d6f9a70f5
Ernad Husremovic <hernad@bring.out.ba>
2016-03-08 16:50:34 +0100

    build

---
+++ scripts/build_gz_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function usage() {
+  echo "primjer: $0 Ubuntu_i686 1.7.750"
+  echo "         $0 Windows 1.7.750"
+  echo "         $0 MacOSX 1.7.750"
+}
+
+[ -z "$1" ] && echo "set envar F18_TYPE argument 1"  && usage && exit 1
+[ -z "$2" ] && echo "set envar F18_VER argument 2"  && usage && exit 1
+
+F18_TYPE=$1
+F18_VER=$2
+
+./build.sh && scripts/build_gz.sh $F18_VER && scripts/push_to_downloads.sh F18_${F18_TYPE}_${F18_VER}.gz 
+

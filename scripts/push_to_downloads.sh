1107f7f56836610b61db237e075869a9b76497c1
Ernad Husremovic <hernad@bring.out.ba>
2016-07-26 10:25:54 +0200

    106 docker host

--- scripts/push_to_downloads.sh
+++ scripts/push_to_downloads.sh
@@ -6,8 +6,7 @@
 #scp $SSH_OPTS $1 root@download.bring.out.ba:$WWW_PATH/
 #ssh $SSH_OPTS root@download.bring.out.ba chmod +r $WWW_PATH/*
 
-#GREEN1=docker@192.168.168.107
-GREEN1=docker@192.168.169.128
+GREEN1=docker@192.168.168.106
 
 scp $1 $GREEN1:/data/f18-downloads/downloads.bring.out.ba/www/files/
 

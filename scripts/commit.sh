41d5d85f595cac72d5c51b3c5b36ecab14990c20
hernad <hernad@bring.out.ba>
2017-06-09 03:04:25 +0200

    publish nver 2.3.520

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -10,7 +10,7 @@ fi
 NEW_VER=$1
 
 sed -i -e "s/f18=.*/f18=$NEW_VER/" UPDATE_INFO 
-git commit -a -m "publish nver $NEW_VER"
 echo $NEW_VER > VERSION
+git commit -a -m "publish nver $NEW_VER"
 git tag $NEW_VER
 git push origin $BRANCH --tags

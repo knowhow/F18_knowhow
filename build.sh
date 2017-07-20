1c8ba7038253cc4038ab9f6f67b71fc2ea029af8
hernad <hernad@bring.out.ba>
2017-07-20 16:25:11 +0200

    update build.sh arg 2 F18_VER

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ if [ "$1" != "--no-rm" ] ; then
    rm -r -f .h
 fi
 
+F18_VER=$2
+
 unset F18_DEBUG
 unset F18_DEBUG_BROWSE_SIF
 unset F18_DEBUG_FIN_AZUR
@@ -16,11 +18,8 @@ unset F18_DEBUG_THREAD
 unset F18_DEBUG_SYNC
 unset F18_DEBUG_SQL
 
-#./build_lib.sh
-
-scripts/update_f18_ver_ch.sh
+scripts/update_f18_ver_ch.sh $F18_VER
 
 [ $? != 0 ] && echo "error git version BUILD ERROR" && exit 1
 
 hbmk2 -workdir=.h F18.hbp
-
--- build_lib.sh
+++
@@ -1,7 +0,0 @@
-if [ "$HB_INC_INSTALL" == "" ]; then
-    echo "setuj envars"
-    exit -1
-fi
-
-
-

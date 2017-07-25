e7a41d2b0fc58e6e2a9b5f03d0fa3192fe5cecce
hernad <hernad@bring.out.ba>
2017-07-25 21:03:43 +0200

    merge_3.sh

--- scripts/merge_3.sh
+++ scripts/merge_3.sh
@@ -2,7 +2,7 @@
 
 git merge --no-ff --no-commit 3
 
-for f $ VERSION VERSION_E VERSION_X script/commit.sh
+for f in VERSION VERSION_E VERSION_X script/commit.sh
       echo "git checkout $f"
       git checkout $f
 done

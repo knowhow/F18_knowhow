32a0bbf9e13de43dc41335879a47811479018439
hernad <hernad@bring.out.ba>
2017-07-26 14:24:28 +0200

    ne diraj kalk_imp_txt vindi

--- scripts/merge_3.sh
+++ scripts/merge_3.sh
@@ -4,7 +4,11 @@ MY_BRANCH=origin/3-vindi
 
 git merge --no-ff --no-commit 3
 
-for f in VERSION VERSION_E VERSION_X include/f18.ch kalk/kalk_import_racuni.prg kalk/kalk_import_partn_roba.prg kalk/kalk_mnu_razmjena_podataka.prg
+for f in VERSION VERSION_E VERSION_X \
+   include/f18.ch \
+   kalk/kalk_imp_txt_racuni.prg  \
+   kalk/kalk_imp_txt_roba_partn.prg \ 
+   kalk/kalk_mnu_razmjena_podataka.prg
 do
       echo "git checkout $MY_BRANCH -- $f"
       git checkout $MY_BRANCH -- $f

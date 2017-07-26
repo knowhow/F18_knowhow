1e648df9b39f1ed8e1ade3645235b1c791942d18
hernad <hernad@bring.out.ba>
2017-07-26 08:27:48 +0200

    merge from 3-vindi

--- scripts/merge_from_3-vindi.sh
+++ scripts/merge_from_3-vindi.sh
@@ -2,11 +2,13 @@
 
 git merge --no-ff --no-commit 3-vindi
 
-for f in VERSION VERSION_E VERSION_X script/commit.sh
-do
-      echo "git checkout $f"
-      git checkout 3 -- $f 
-done
+#for f in VERSION VERSION_E VERSION_X script/commit.sh
+#do
+#      echo "git checkout $f"
+#      git checkout origin/3 -- $f 
+#done
+
+rm VERSION VERSION_E VERSION_X scripts/commit.sh
 
 git status
 

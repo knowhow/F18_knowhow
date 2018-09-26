8789cad901595ab35ef43406ca24b4615e1809c5
Ernad Husremovic <hernad@bring.out.ba>
2018-09-26 13:24:29 +0200

    F18/.vimrc

--- bin/F18_install.sh
+++ bin/F18_install.sh
@@ -129,7 +129,57 @@ cat > f18_editor <<- EOM
 cat \$1 | sed 's/#%.*#//g' | iconv -c -f IBM852 -t UTF-8 > \$1.conv.txt
 
 export BANG=\!  
-gvim -c "nmap <C-P> :exe '\$BANG ptxt ' . substitute(@%, '.conv.txt', '', 'y') . ' /p'<CR>" \$1.conv.txt
+gvim -u ${HOME}/F18/.vimrc -c "nmap <C-P> :exe '\$BANG ptxt ' . substitute(@%, '.conv.txt', '', 'y') . ' /p'<CR>" \$1.conv.txt
+EOM
+
+
+cat > ${HOME}/F18/.vimrc <<- EOM
+set nocompatible
+
+set diffexpr=MyDiff()
+
+" Use pathogen to easily modify the runtime path to include all plugins under
+" the ~/.vim/bundle directory
+filetype off                    " force reloading *after* pathogen loaded
+
+
+set hidden
+set nowrap        " don't wrap lines
+set tabstop=4     " a tab is four spaces
+set backspace=indent,eol,start
+                  " allow backspacing over everything in insert mode
+set shiftwidth=4  " number of spaces to use for autoindenting
+set shiftround    " use multiple of shiftwidth when indenting with '<' and '>'
+set showmatch     " set show matching parenthesis
+set ignorecase    " ignore case when searching
+set smartcase     " ignore case if search pattern is all lowercase,
+
+set history=1000         " remember more commands and search history
+set undolevels=1000      " use many muchos levels of undo
+set wildignore=*.swp,*.bak,*.pyc,*.class
+set title                " change the terminal's title
+set visualbell           " don't beep
+set noerrorbells         " don't beep
+
+set nobackup
+set noswapfile
+
+" Easy window navigation
+map <C-h> <C-w>h
+map <C-j> <C-w>j
+map <C-k> <C-w>k
+map <C-l> <C-w>l
+
+set fileencoding=utf-8
+set encoding=utf-8
+
+"izadlji sa esc
+nmap <ESC> :q!<CR>
+set columns=9999 lines=9999
+set guioptions+=b
+set guioptions-=R
+set guioptions-=m  "remove menu bar
+set guioptions-=T  "remove toolbar
 EOM
 
 chmod +x F18.sh

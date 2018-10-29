268fd7cbe6b623824ec7bfb42e16b7606b7786b7
Ernad Husremovic <hernad@bring.out.ba>
2018-10-29 15:24:12 +0100

    rhel_set_envars.sh

---
+++ scripts/rhel_set_envars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LX_RHEL=1
+
+. scripts/linux.sh
+
+. scripts/set_envars.sh
+
+for m in $MODULES
+do
+    HB_DBG_PATH="$HB_DBG_PATH:$(pwd)/$m"
+done
+
+export HB_DBG_PATH
+echo "HB_DBG_PATH="  $HB_DBG_PATH
+
+export GT_DEFAULT_XWC=1
+
+export F18_POS=1
+export F18_DEBUG=1
+
+echo "-------------- F18 envars ---------------------"
+set | grep ^F18_

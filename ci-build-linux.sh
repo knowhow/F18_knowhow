01d28a6e1d49227f1877b6c85eed86117ba24a7f
Ernad Husremovic <hernad@bring.out.ba>
2019-01-11 20:26:47 +0100

    sudo dpkg --add-architecture i386

--- ci-build-linux.sh
+++ ci-build-linux.sh
@@ -8,27 +8,38 @@ gcc --version
 
 # https://redmine.bring.out.ba/issues/35387
 
-# sudo dpkg --add-architecture i386
+
+export HB_PLATFORM=linux
+
 
 if [ "$BUILD_ARCH" == "ia32" ] ; then
+
+   sudo dpkg --add-architecture i386
+   sudo apt install -y g++-multilib gcc-multilib libc6:i386 \
+     libx11-dev:i386 libpcre3-dev:i386 libssl-dev:i386 \
+     libncurses5:i386 libstdc++6:i386 lib32stdc++6  libpq-dev:i386 lib32z1
+
    curl -L https://dl.bintray.com/hernad/harbour/hb-linux-i386.tar.gz > hb.tar.gz
    tar xvf hb.tar.gz
+
+   export HB_USER_CFLAGS=-m32
+   export HB_USER_DFLAGS='-m32 -L/usr/lib32'
+   export HB_USER_LDFLAGS='-m32 -L/usr/lib32'
+    
+   export HB_ROOT=$(pwd)/hb-linux-i386
+
 else
+    sudo apt-get update -y
+    sudo apt-get install -y g++ gcc libc6 \
+      libx11-dev libpcre3-dev libssl-dev \
+      libncurses5 libstdc++6  libpq-dev lib32z1
+
+
    exit 1
 fi
 
 set
 
-export HB_PLATFORM=linux
-export HB_ROOT=$(pwd)/hb-linux-i386
-#export HB_USER_CFLAGS=-m32
-#export HB_USER_DFLAGS='-m32 -L/usr/lib32'
-#export HB_USER_LDFLAGS='-m32 -L/usr/lib32'
-
-sudo apt-get update -y
-sudo apt-get install -y g++ gcc libc6 \
-     libx11-dev libpcre3-dev libssl-dev \
-     libncurses5 libstdc++6  libpq-dev lib32z1
 
 PATH=$HB_ROOT/bin:$PATH
 

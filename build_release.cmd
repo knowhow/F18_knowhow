1e740127316ceb6ec1b280aa06474bffe4b54ef8
Ernad Husremovic <hernad@bring.out.ba>
2019-11-18 13:58:06 +0100

    build windows

---
+++ build_release.cmd
@@ -0,0 +1,3 @@
+C:\msys64\usr\bin\bash -lc "export BINTRAY_ARCH=x64 ; export HB_COMPILER=mingw64 ; export F18_ESHELL=1 ; cd /c/Users/hernad/F18 ; bash ci-build-win32.sh"
+
+move F18.exe F18_release.exe

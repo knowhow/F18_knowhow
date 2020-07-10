b9bafc4c199a14b443696145eab75e63a6bb0364
Ernad Husremovic <hernad@bring.out.ba>
2020-07-10 08:50:17 +0200

    bild windows release

--- build_release.cmd
+++ build_release.cmd
@@ -1,3 +1,19 @@
-C:\msys64\usr\bin\bash -lc "export BINTRAY_ARCH=x64 ; export HB_COMPILER=mingw64 ; export F18_ESHELL=1 ; cd /c/Users/hernad/F18 ; bash ci-build-win32.sh"
+set F18_POS=1
+set VERSION=3.1.128
+set DATE=10.07.2020
 
-move F18.exe F18_release.exe
+copy /Y include\f18_ver_template.ch include\f18_ver.ch
+
+echo #define F18_VER       "%VERSION%" >> include\f18_ver.ch
+echo #define F18_VER_DATE  "%DATE%" >> include\f18_ver.ch
+
+type include\f18_ver.ch
+
+
+hbmk2 F18 -clean
+hbmk2 F18 -trace- -ldflag+=/NODEFAULTLIB:LIBCMT
+
+copy F18.exe F18_Windows_%VERSION%
+c:\cygwin64\bin\gzip F18_Windows_%VERSION%
+
+dir F18_Windows_%VERSION%.gz

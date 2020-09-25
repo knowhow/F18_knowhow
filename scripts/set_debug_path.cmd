698ae066b3703d0122f87413fc9f8315b8f59e3e
Ernad Husremovic <hernad@bring.out.ba>
2020-09-25 12:33:16 +0200

    set_debug_path.cmd

---
+++ scripts/set_debug_path.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+set HB_DBG=C:\dev\F18_mono\F18_v3
+set HB_DBG_PATH=%HB_DBG%\common;%HB_DBG%\pos;%HB_DBG%\kalk;%HB_DBG%\fin
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\fakt
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\os
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\ld
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\virm
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\core
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\core_sql
+set HB_DBG_PATH=%HB_DBG_PATH%;%HB_DBG%\core_pdf
+
+
+echo HB_DBG_PATH=%HB_DBG_PATH%
+

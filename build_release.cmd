f4d662ec3554fdf666581de9bd8faf5cb42a9ab1
Ernad Husremovic <hernad@bring.out.ba>
2020-12-31 13:02:47 +0100

    cleanup 3.3.6

--- build_release.cmd
+++ build_release.cmd
@@ -6,7 +6,7 @@ set HB_COMPILER=msvc
 set F18_DEBUG=
 set F18_POS=1
 set DATE=31.12.2020
-set VERSION=3.3.5
+set VERSION=3.3.6
 
 IF [%VERSION%]==[] (
    echo ENVAR VERSION nije definisana. STOP!
@@ -21,10 +21,10 @@ echo #define F18_VER_DATE  "%DATE%" >> include\f18_ver.ch
 type include\f18_ver.ch
 
 
-hbmk2 F18 -clean -workdir=.b32
+REM hbmk2 F18 -clean -workdir=.b32
 hbmk2 F18 -trace- -ldflag+=/NODEFAULTLIB:LIBCMT -workdir=.b32
 
-
+copy /y F18.exe F18_2.exe
 c:\cygwin64\bin\gzip --force F18.exe
 
 echo pravim F18_Windows_%VERSION%.gz ...

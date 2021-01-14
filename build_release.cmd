fdd0db4118e5c46d0c8d2431f27d11fdc38a4db5
Ernad Husremovic <hernad@bring.out.ba>
2021-01-14 08:56:39 +0100

    3.3.12

--- build_release.cmd
+++ build_release.cmd
@@ -5,8 +5,8 @@ set HB_COMPILER=msvc
 
 set F18_DEBUG=
 set F18_POS=1
-set DATE=06.01.2021
-set VERSION=3.3.11
+set DATE=14.01.2021
+set VERSION=3.3.12
 
 IF [%VERSION%]==[] (
    echo ENVAR VERSION nije definisana. STOP!
